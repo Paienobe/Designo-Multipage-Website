d128e8ba6c585a096385ba9af7a975df0ed6c406
unknown <paienobe@gmail.com>
2022-05-30 15:34:42 +0100

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,46 @@
 import React from 'react'
 import './Footer.css'
-import footerLogo from '../../assets/shared/desktop/logo-dark.png'
+import footerLogo from '../../assets/shared/desktop/logo-light.png'
+import facebook from '../../assets/shared/desktop/icon-facebook.svg'
+import youtube from '../../assets/shared/desktop/icon-youtube.svg'
+import twitter from '../../assets/shared/desktop/icon-twitter.svg'
+import pinterest from '../../assets/shared/desktop/icon-pinterest.svg'
+import instagram from '../../assets/shared/desktop/icon-instagram.svg'
 
 const Footer = () => {
+  const socialIcons = [facebook, youtube, twitter, pinterest, instagram]
   return (
     <footer>
-      <img src={footerLogo} alt='logo' />
+      <img src={footerLogo} alt='logo' className='footer_logo' />
+      <hr />
+
+      <div className='footer_links'>
+        <p>OUR COMPANY</p>
+        <p>LOCATIONS</p>
+        <p>CONTACT</p>
+      </div>
+
+      <address>
+        <p className='title'>Designo Central Office</p>
+        <p>3886 Wellington Street</p>
+        <p>Toronto, Ontario M9C 3J5</p>
+      </address>
+
+      <div className='contact_information'>
+        <p className='title'>Contact Us (Central Office)</p>
+        <p>P: +1 253-863-8967</p>
+        <p>M: contact@designo.co</p>
+      </div>
+
+      <div className='socials'>
+        {socialIcons.map((icon, index) => {
+          return (
+            <a href='#'>
+              <img src={icon} alt='social_icon' />
+            </a>
+          )
+        })}
+      </div>
     </footer>
   )
 }
