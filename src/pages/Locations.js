a9f30d8605c6ac14a4395af61a68c2b72d03bd95
unknown <paienobe@gmail.com>
2022-05-31 14:40:18 +0100

    styled locations page

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import ContactCard from '../components/Contact Card/ContactCard'
+import LocationDetails from '../components/Location Details/LocationDetails'
 
 const Locations = () => {
-  return <div>Locations</div>
+  return (
+    <div className='locations'>
+      <LocationDetails />
+      <ContactCard />
+    </div>
+  )
 }
 
 export default Locations
